7873392339dea05dc6fa2f996ad4a23ba3912794
Ast <varistyshark@gmail.com>
2025-05-16 10:05:07 -0400

    added deploy bash script and fixed .toml

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,2 +1,1 @@
-forge script --chain base_sepolia -sepolia script/PensionVault.s.sol --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --private_key $BASE_SEPOLIA_PRIVATE_KEY -vvvv --interactives 1
-forge script --chain base_sepolia -sepolia script/PensionVault.s.sol --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast -vvvv --interactives 1
+ forge script  script/PensionVault.s.sol --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast -vvvv --private-key $PRIVATE_KEY --verify 
